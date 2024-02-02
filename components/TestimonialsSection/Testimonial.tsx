1518b7d7254ab01c8e2c50307f63e5c8a2e9f293
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-03 02:13:08 +0530

    updated testimonials

--- components/TestimonialsSection/Testimonial.tsx
+++ components/TestimonialsSection/Testimonial.tsx
@@ -5,41 +5,92 @@ interface TestimonialProps {
     body: string
     name: string
     email: string
+    pfp?: string
+    linkedin?: string
+    github?: string
+    instagram?: string
+    dribbble?: string
+    phone?: string
 }
 
-function Testimonial({ body, name, email }: TestimonialProps) {
+function Testimonial({
+    body,
+    name,
+    email,
+    pfp,
+    dribbble,
+    github,
+    instagram,
+    linkedin,
+    phone,
+}: TestimonialProps) {
     return (
         <figure className="rounded-2xl bg-background shadow-lg ring-1 ring-primary flex flex-col">
             <blockquote className="p-6 text-lg font-semibold leading-7 tracking-tight sm:p-12 sm:text-xl sm:leading-8 flex-1">
                 <p>{`“${body}”`}</p>
             </blockquote>
             <figcaption className="flex flex-wrap items-center gap-x-4 gap-y-4 border-t border-primary/10 px-6 py-4 sm:px-12 lg:flex-nowrap">
-                {/* <img
-                    className="h-10 w-10 flex-none rounded-full bg-gray-50"
-                    src={featuredTestimonial.author.imageUrl}
-                    alt=""
-                /> */}
+                {pfp && (
+                    <img
+                        className="h-10 w-10 flex-none rounded-full bg-gray-50"
+                        src={pfp}
+                        alt=""
+                    />
+                )}
                 <div className="flex-auto">
                     <div className="font-semibold">{name}</div>
                     <div className="text-muted">{`${email}`}</div>
                 </div>
-                {/* <div className="links flex items-center gap-1 text-primary">
-                    <a className="rounded-full hover:bg-secondary p-2" href="">
-                        <Instagram />
-                    </a>
-                    <a className="rounded-full hover:bg-secondary p-2" href="">
-                        <Linkedin />
-                    </a>
-                    <a className="rounded-full hover:bg-secondary p-2" href="">
-                        <GitHub />
-                    </a>
-                    <a className="rounded-full hover:bg-secondary p-2" href="">
-                        <Dribble />
-                    </a>
-                    <a className="rounded-full hover:bg-secondary p-2" href="">
-                        <PhoneCall />
-                    </a>
-                </div> */}
+                <div className="links flex items-center gap-1 text-primary">
+                    {instagram && (
+                        <a
+                            target="_blank"
+                            referrerPolicy="no-referrer"
+                            className="rounded-full hover:bg-secondary p-2"
+                            href={instagram}
+                        >
+                            <Instagram />
+                        </a>
+                    )}
+                    {linkedin && (
+                        <a
+                            target="_blank"
+                            referrerPolicy="no-referrer"
+                            className="rounded-full hover:bg-secondary p-2"
+                            href={linkedin}
+                        >
+                            <Linkedin />
+                        </a>
+                    )}
+                    {github && (
+                        <a
+                            target="_blank"
+                            referrerPolicy="no-referrer"
+                            className="rounded-full hover:bg-secondary p-2"
+                            href={github}
+                        >
+                            <GitHub />
+                        </a>
+                    )}
+                    {dribbble && (
+                        <a
+                            target="_blank"
+                            referrerPolicy="no-referrer"
+                            className="rounded-full hover:bg-secondary p-2"
+                            href={dribbble}
+                        >
+                            <Dribbble />
+                        </a>
+                    )}
+                    {phone && (
+                        <a
+                            className="rounded-full hover:bg-secondary p-2"
+                            href=""
+                        >
+                            <PhoneCall />
+                        </a>
+                    )}
+                </div>
             </figcaption>
         </figure>
     )
--- configs/testimonials.ts
+++ configs/testimonials.ts
@@ -3,6 +3,11 @@ const testimonials = [
         body: "Chirag was amazing! He took my ideas and made them into a fantastic website. I'm not good with tech stuff, but he explained everything so well. Now, my website is way better than I imagined! Totally recommend!",
         name: 'Madhur Chopra',
         email: 'madhurchopra97441@gmail.com',
+        pfp: '/pfps/madhur.PNG',
+        linkedin: 'https://in.linkedin.com/in/madhur-chopra-ux',
+        instagram:
+            'https://www.instagram.com/madhur_chopra?igsh=MXVhZzljc3BsdzB6dg%3D%3D&utm_source=qr',
+        dribbble: 'https://dribbble.com/madhur-chopra-ux',
     },
     {
         name: 'Deep Agarwal',
@@ -10,8 +15,8 @@ const testimonials = [
         body: "Chirag is a coding pro! He's really good with React.js and backend stuff. We did a tough project, and he turned our tech needs into a smooth web solution. Excited for more projects with him!",
     },
     {
-        name: "Anshul Kalsain",
-        email: "anshulans6@gmail.com",
+        name: 'Anshul Kalsain',
+        email: 'anshulans6@gmail.com',
         body: "Chirag made everything easy! I don't know tech things, but he was patient, answered all my questions, and made a website that's perfect for my business. He's super skilled and patient. Thanks for making my vision real!!",
     },
 ]
