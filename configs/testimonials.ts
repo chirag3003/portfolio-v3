7416acf12f6a28f4985eb0cf63fe8cfca9b45d78
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-08 21:57:08 +0530

    added a testimonial

--- configs/testimonials.ts
+++ configs/testimonials.ts
@@ -19,5 +19,13 @@ const testimonials = [
         email: 'anshulans6@gmail.com',
         body: "Chirag made everything easy! I don't know tech things, but he was patient, answered all my questions, and made a website that's perfect for my business. He's super skilled and patient. Thanks for making my vision real!!",
     },
+    {
+        body: "Chirag is different from other developers. His skills in Next.js and Golang are top-notch. He doesn't just write code; he makes digital magic. Working with him isn't just a project; it's a journey of cool ideas and awesome results!",
+        name: 'Dhruv Pruthi',
+        email: 'frozendecember2001@gmail.com',
+        pfp: '/pfps/para.jpg',
+        github: 'https://github.com/para-docx',
+        linkedin: 'https://www.linkedin.com/in/para-docx/',
+    },
 ]
 export default testimonials
