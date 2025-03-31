618115c1ecd5726fc0244c3d83f03c304439e6f0
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2025-03-31 22:44:04 +0530

    updated projects redirect link

--- components/HeroSection/projectButton.tsx
+++ components/HeroSection/projectButton.tsx
@@ -1,25 +1,29 @@
 'use client'
 
 import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
 import { useToast } from '@/components/ui/use-toast'
 import React from 'react'
+import Link from 'next/link'
 
 function ProjectButton() {
     const { toast } = useToast()
     return (
-        <Button
-            size={'lg'}
-            className={cn('font-bold text-xl md:w-auto w-full')}
-            onClick={() => {
-                toast({
-                    title: 'Projects page coming soon!',
-                    description: 'Till then you can take a look at my github',
-                })
-            }}
+        <Link
+            className={cn(
+                buttonVariants({ size: 'lg' }),
+                'font-bold text-xl md:w-auto w-full'
+            )}
+            href={"https://projects.chirag.codes"}
+            //onClick={() => {
+            //    toast({
+            //        title: 'Projects page coming soon!',
+            //        description: 'Till then you can take a look at my github',
+            //    })
+            //}}
         >
             My Projects
-        </Button>
+        </Link>
     )
 }
 
